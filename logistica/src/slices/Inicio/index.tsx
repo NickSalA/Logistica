f800f62f0962a5ea0a04b6a9acd45f41a2b331cf
Nick <nicksalcedo717@gmail.com>
2025-03-24 18:20:50 -0500

    Conexión a Prismic #2 - Corrección -  Slices

--- logistica/src/slices/Inicio/index.tsx
+++ logistica/src/slices/Inicio/index.tsx
@@ -17,14 +17,21 @@ const Inicio: FC<InicioProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
+      <div className="w-1/4 opacity-80 bg-night rounded-lg" >
       <PrismicRichText field={slice.primary.main_info} components={{
         heading1: ({children}) => (
-          <h1 className="font-montserrat text-7xl">{children}</h1>
+          <h1 className="font-primary text-5xl text-white">{children}</h1>
+        ) 
+      }}/>
+      <PrismicRichText field={slice.primary.name} components={{
+        heading1: ({children}) => (
+          <h1 className="font-primary text-5xl text-white">{children}</h1>
         )
       }}/>
-      <PrismicRichText field={slice.primary.name} />
-      <PrismicNextLink field={slice.primary.button_right_link}>
-      {slice.primary.button_right}
+      </div>
+      
+      <PrismicNextLink field={slice.primary.button_right_link} className="top 1/2 ">
+        {slice.primary.button_right}
       </PrismicNextLink>
       <PrismicNextLink field={slice.primary.button_left_link}>
       {slice.primary.button_left}
